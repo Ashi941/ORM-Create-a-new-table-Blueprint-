4396dcf07d00881ecd279cf7abc95e46e2e1e56f
Asima Pal <asima.pal@experionglobal.local>
2024-02-08 21:47:11 +0530

    feat: new commits

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 
 db = SQLAlchemy()
@@ -9,8 +10,10 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:experion%40123@localhost/training"
-
+    app.config['JWT_SECRET_KEY'] = "abcd"
     db.init_app(app)
+    #initialising jwt 
+    jwt=JWTManager(app)
     from app.user import user_bp
     app.register_blueprint(user_bp)
     return app
